f9d5029504987dfd082379c700c298611c7e9bf5
Jessica Häseli <jessica.haeseli@stud.fhgr.ch>
2025-05-07 15:25:11 +0200

    Fetch Silly, Brainrot

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+async function loadSilly() {
+    const url = 'https://official-joke-api.appspot.com/jokes/random/1'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const dataSilly = await loadSilly();
+console.log(dataSilly); // gibt die Daten der API oder false in der Konsole aus
+
+async function loadBrainrot() {
+    const url = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const dataBrainrot = await loadBrainrot();
+console.log(dataBrainrot); // gibt die Daten der API oder false in der Konsole aus
+
