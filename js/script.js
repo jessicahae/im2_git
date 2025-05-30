d2d240f29b4c69979b077eb68ddc93d3c8765671
Jessica Häseli <jessica.haeseli@stud.fhgr.ch>
2025-05-30 18:14:39 +0200

    clean up js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* fetch APIs */
+
 async function loadSilly() {
     const url = 'https://official-joke-api.appspot.com/jokes/random/1'; // mit korrekter API-URL ersetzen
     try {
@@ -37,86 +39,7 @@ async function loadPositive() {
 const dataPositive = await loadPositive();
 console.log(dataPositive); // gibt die Daten der API oder false in der Konsole aus
 
-const button_happy = document.querySelector('#happy');
-const button_sad = document.querySelector('#sad');
-const button_anxious = document.querySelector('#anxious');
-const button_bored = document.querySelector('#bored');
-const button_next = document.querySelector('#next-mood-question');
-const image_face = document.querySelector('.face');
-const selection_moodbiter = document.querySelector('#mood-biter');
-const selection_moodquestion = document.querySelector('#mood-question');
-
-button_happy.addEventListener('click', function () {
-    button_next.classList.remove('hidden');
-    image_face.src = 'img/Face_Happy.svg';
-});
-
-button_sad.addEventListener('click', function () {
-    button_next.classList.remove('hidden');
-    image_face.src = 'img/Face_Sad.svg';
-});
-
-button_anxious.addEventListener('click', function () {
-    button_next.classList.remove('hidden');
-    image_face.src = 'img/Face_Anxious.svg';
-});
-
-button_bored.addEventListener('click', function () {
-    button_next.classList.remove('hidden');
-    image_face.src = 'img/Face_Bored.svg';
-});
-button_next.addEventListener('click', function () {
-    selection_moodbiter.classList.remove('hidden');
-    selection_moodquestion.classList.add('hidden');
-});
-
-const button_silly = document.querySelector('#silly');
-const button_brainrot = document.querySelector('#brainrot');
-const button_positive = document.querySelector('#positive');
-const button_next_silly = document.querySelector('#next-silly');
-const selection_brainrot = document.querySelector('#mood-brainrot');
-const selection_silly = document.querySelector('#mood-silly');
-const selection_positive = document.querySelector('#mood-positive');
-
-button_brainrot.addEventListener('click', function () {
-    selection_brainrot.classList.remove('hidden');
-    selection_moodbiter.classList.add('hidden');
-});
-
-button_silly.addEventListener('click', function () {
-    selection_silly.classList.remove('hidden');
-    selection_moodbiter.classList.add('hidden');
-});
-
-button_positive.addEventListener('click', function () {
-    selection_positive.classList.remove('hidden');
-    selection_moodbiter.classList.add('hidden');
-});
-
-const div_brainrot = document.querySelector('.mood-brainrot-text-container');
-
-document.querySelector('.mood-brainrot-text').innerHTML = dataBrainrot.text;
-
-const brainrot_button_next = document.querySelector('#mood-brainrot-button');
-const brainrot_text = document.querySelector('.mood-brainrot-text');
-
-// brainrot_button_next.addEventListener('click', function () {
-
-// });
-
-const loadNewFact = () => {
-    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
-        .then(response => response.json())
-        .then(data => {
-            brainrot_text.innerHTML = data.text; 
-        })
-        .catch(error => {
-            console.error('Fehler beim Laden des Fun Facts:', error);
-           brainrot_text.innerHTML = "Oops, konnte keinen neuen Fakt laden.";
-        });
-};
-loadNewFact();
-brainrot_button_next.addEventListener('click', loadNewFact);
+/* General adjustments */
 
 const allLogos = document.querySelectorAll('.logo');
 
@@ -167,15 +90,99 @@ changeMoodButtons.forEach(button => {
     });
 });
 
+/* Starter page mood question */
+
+const button_happy = document.querySelector('#happy');
+const button_sad = document.querySelector('#sad');
+const button_anxious = document.querySelector('#anxious');
+const button_bored = document.querySelector('#bored');
+const button_next = document.querySelector('#next-mood-question');
+const image_face = document.querySelector('.face');
+const section_moodbiter = document.querySelector('#mood-biter');
+const section_moodquestion = document.querySelector('#mood-question');
+
+button_happy.addEventListener('click', function () {
+    button_next.classList.remove('hidden');
+    image_face.src = 'img/Face_Happy.svg';
+});
+
+button_sad.addEventListener('click', function () {
+    button_next.classList.remove('hidden');
+    image_face.src = 'img/Face_Sad.svg';
+});
+
+button_anxious.addEventListener('click', function () {
+    button_next.classList.remove('hidden');
+    image_face.src = 'img/Face_Anxious.svg';
+});
+
+button_bored.addEventListener('click', function () {
+    button_next.classList.remove('hidden');
+    image_face.src = 'img/Face_Bored.svg';
+});
+button_next.addEventListener('click', function () {
+    section_moodbiter.classList.remove('hidden');
+    section_moodquestion.classList.add('hidden');
+});
+
+/* transfer to different moods */
+
+const button_silly = document.querySelector('#silly');
+const button_brainrot = document.querySelector('#brainrot');
+const button_positive = document.querySelector('#positive');
+const button_next_silly = document.querySelector('#next-silly');
+const section_brainrot = document.querySelector('#mood-brainrot');
+const section_silly = document.querySelector('#mood-silly');
+const section_positive = document.querySelector('#mood-positive');
+
+button_brainrot.addEventListener('click', function () {
+    section_brainrot.classList.remove('hidden');
+    section_moodbiter.classList.add('hidden');
+});
+
+button_silly.addEventListener('click', function () {
+    section_silly.classList.remove('hidden');
+    section_moodbiter.classList.add('hidden');
+});
+
+button_positive.addEventListener('click', function () {
+    section_positive.classList.remove('hidden');
+    section_moodbiter.classList.add('hidden');
+});
+
+/* Page brainrot */
+
+const div_brainrot = document.querySelector('.mood-brainrot-text-container');
+const brainrot_button_next = document.querySelector('#mood-brainrot-button');
+const brainrot_text = document.querySelector('.mood-brainrot-text');
+
+document.querySelector('.mood-brainrot-text').innerHTML = dataBrainrot.text;
+
+const loadNewFact = () => {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
+        .then(response => response.json())
+        .then(data => {
+            brainrot_text.innerHTML = data.text; 
+        })
+        .catch(error => {
+            console.error('Fehler beim Laden des Fun Facts:', error);
+           brainrot_text.innerHTML = "Oops, konnte keinen neuen Fakt laden.";
+        });
+};
+loadNewFact();
+brainrot_button_next.addEventListener('click', loadNewFact);
+
+
+/* Page silly */
+
 const div_silly = document.querySelector('.mood-silly-container');
-
-document.querySelector('.mood-silly-text').innerHTML = dataBrainrot.text;
-
 const silly_button_next = document.querySelector('#mood-silly-button');
 const silly_text = document.querySelector('.mood-silly-text');
 const silly_text2 = document.querySelector('#mood-silly-text-answer');
 const imStupid_button = document.querySelector('#im-stupid');
 const sillyTextAnswer = document.querySelector('#silly-text-answer');
+
+document.querySelector('.mood-silly-text').innerHTML = dataBrainrot.text;
 
 const loadNewFactSilly = () => {
     fetch('https://official-joke-api.appspot.com/jokes/random/1')
@@ -196,6 +203,8 @@ silly_button_next.addEventListener('click', loadNewFactSilly);
 imStupid_button.addEventListener('click', function () {
     sillyTextAnswer.classList.remove('hidden');
 });
+
+/* Page positive */
 
 const div_positive = document.querySelector('.mood-positive-container');
 const positive_text = document.querySelector('.mood-positive-text');
@@ -225,12 +234,15 @@ positive_button_next.addEventListener('click', function (){
     loadNewFactPositive();
 });
 
+/* Pages mood done (finish/done) */
+
 const button_better = document.querySelector('#button-mood-better');
 const button_worse = document.querySelector('#button-mood-worse');
 const button_feel = document.querySelector('#button-mood-feel');
 const section_moodFinish = document.querySelector('#mood-finish');
 const section_moodDone = document.querySelector('#mood-done');
 const section_moodFailed = document.querySelector('#mood-failed');
+const button_biteAgain = document.querySelector('#mood-failed-button');
 
 button_better.addEventListener('click', function () {
     section_moodDone.classList.remove('hidden');
@@ -244,11 +256,9 @@ button_better.addEventListener('click', function () {
     });
 });
 
-const button_biteAgain = document.querySelector('#mood-failed-button');
-
 button_biteAgain.addEventListener('click', function () {
     section_moodFailed.classList.add('hidden');
-    selection_moodbiter.classList.remove('hidden');
+    section_moodbiter.classList.remove('hidden');
 });
 
 
